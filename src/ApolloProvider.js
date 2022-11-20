97e03b13862c7a63225f1a41c9d091886da9ebee
alejosilvalau <alejo.lautaro@hotmail.com>
2022-11-20 14:34:38 -0300

    changed server uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-	uri: "https://comment-me.herokuapp.com/",
+	uri: "https://comment-me-api.onrender.com/",
 });
 
 const authLink = setContext(() => {
